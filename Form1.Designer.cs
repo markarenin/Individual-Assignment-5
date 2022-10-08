e82238822357496973d904a6413bf83af9f85dcc
Adam Alidibirov <85851465+markarenin@users.noreply.github.com>
2022-10-08 12:27:07 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Individual_Assignment_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(215, 188);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(180, 31);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDown1_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(430, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hi! I guessed a number between 1-100\r\ntry to guess or your computer will turn int" +
+    "o a cake :D";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(301, 310);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Attempts to guess: 0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(421, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Location = new System.Drawing.Point(376, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 423);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Label label3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿namespace Individual_Assignment_5
+{
+    public partial class Form1 : Form
+    {
+        private int random_int;
+        private int tries = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            newgame();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            check((int)numericUpDown1.Value);
+        }
+
+        private void check(int value)
+        {
+            tries++;
+            if (value == random_int)
+            {
+                MessageBox.Show($"Wow! Congratulations, you guessed the number! You computer will not turn into a cake 😊 \nAttempts to guess: {tries}");
+                tries = 0;
+                newgame();
+            }
+            else if (value > random_int)
+            {
+                label3.Text = "Too high, try again.";
+            }
+            else
+            {
+                label3.Text = "Too low, try again.";
+            }
+            label2.Text = $"Attempts to guess: {tries}";
+            numericUpDown1.Focus();
+            numericUpDown1.Select(0, 3);
+        }
+
+        private void newgame()
+        {
+            random_int = new Random().Next(0, 100);
+            numericUpDown1.Value = 0;
+            label3.Text = "";
+            label2.Text = "Attempts to guess: 0";
+        }
+
+
+        private void numericUpDown1_KeyUp(object sender, KeyPressEventArgs e)
+        {
+
+            if (13 == e.KeyChar)
+            {
+                numericUpDown1.Validate();
+                check((int)numericUpDown1.Value);
+            }
+        }
+    }
+}
